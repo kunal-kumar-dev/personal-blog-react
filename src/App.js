a82bf54c537f8cf2a25a85614fbb320f44649810
Kunal Kumar Rajak <thetechkunal@gmail.com>
2026-01-07 18:52:39 +0530

    Improve full dark mode styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
@@ -6,21 +8,40 @@ import BlogDetails from "./pages/BlogDetails";
 import CreateBlog from "./pages/CreateBlog";
 import EditBlog from "./pages/EditBlog";
 import About from "./pages/About";
+
 import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/:id" element={<BlogDetails />} />
-        <Route path="/create" element={<CreateBlog />} />
-        <Route path="/edit/:id" element={<EditBlog />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <div className={theme}>
+      <BrowserRouter>
+        <Navbar toggleTheme={toggleTheme} theme={theme} />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
+          <Route path="/create" element={<CreateBlog />} />
+          <Route path="/edit/:id" element={<EditBlog />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ toggleTheme, theme }) {
   return (
     <nav className="navbar">
       <h2 className="logo">MyBlog</h2>
+
       <div>
         <Link to="/">Home</Link>
         <Link to="/blogs">Blogs</Link>
         <Link to="/create">Create</Link>
         <Link to="/about">About</Link>
+
+        <button className="theme-btn" onClick={toggleTheme}>
+          {theme === "light" ? "Dark" : "Light"}
+        </button>
       </div>
     </nav>
   );
